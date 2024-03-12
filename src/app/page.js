c10b0ca0373f1dda96f85bd6e70e3b851757125d
Sahil <sahilrohera10@gmail.com>
2024-03-12 16:58:58 +0530

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 
 import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 import SimilarTopics from "@/components/SimilarTopics";
 
 // import Flow from "../components/flow";
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <>
       <div className="w-[90%] m-auto overflow-x-hidden ">
+        <Hero />
         <SimilarTopics />
         <Footer />
       </div>{" "}
